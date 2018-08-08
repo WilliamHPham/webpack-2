538c5bf399fcad90bc8884877ff4314c853979a7
HuyPND <huypnd@smartosc.com>
2018-08-08 15:43:46 +0700

    HTML Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,25 @@
+//const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/app.js',
     output: {
-        filename: './dist/app.bundle.js'
-    }
+        path: __dirname + '/dist',
+        filename: 'app.bundle.js'
+    },
+    plugins:[
+        // Generates default index.html
+        new HtmlWebpackPlugin(),
+        // Generate a index.bundle.html
+        new HtmlWebpackPlugin({
+            title: 'Webpack 2',
+            // minify:
+            // {
+            //     collapseWhitespace: true
+            // },
+            hash: true,
+            filename: './index.bundle.html',
+            template: './src/index.html'
+        })
+    ]
 }
